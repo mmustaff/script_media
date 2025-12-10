b3ee3bbd72705adb7d738eaedb0ae36821771736
Mustaffa, Mustamin B <mustamin.b.mustaffa@intel.com>
2025-12-10 16:43:27 +0800

    Update build_media_driver.sh with install
    Can install packages at the end
    
    Signed-off-by: Mustaffa, Mustamin B <mustamin.b.mustaffa@intel.com>

--- build_media_driver.sh
+++ build_media_driver.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Define log file
+LOG_FILE="build_log.txt"
+
 # Check if the build directory exists
 if [ ! -d "build" ]; then
-	    echo "Creating build directory..."
-	        mkdir build
+	echo "Creating build directory..."
+	mkdir build
+else
+	echo "Deleting existing and recreate build directory"
+	rm -rf build
+	mkdir build
 fi
 
 # Navigate into the build directory
@@ -11,7 +18,22 @@ cd build || exit
 
 # Run cmake and make
 echo "Running cmake..."
-cmake ..
+cmake .. | tee -a "$LOG_FILE"
 
 echo "Building with make..."
-make -j"$(nproc)"
+make -j"$(nproc)" 2>&1 | tee -a "$LOG_FILE"
+
+
+# Check if make was successful
+if [ $? -eq 0 ]; then
+	echo ""
+	read -p "✅ Build completed successfully. Do you want to run 'make install'? (y/n): " choice
+	if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+		echo "Running make install..."
+		sudo make install 2>&1 | tee -a "$LOG_FILE"
+	else
+		echo "Skipping make install."
+	fi
+else
+	echo "❌ Build failed. Check '$LOG_FILE' for details."
+fi
